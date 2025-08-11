524bd52d5a3bbf0270acdf543a54ee8b1f217bb1
mcnuttys <isaacmcnuttys50@gmail.com>
2025-08-11 12:39:31 -0400

    Tweak to allow for ROW COLUMN control

--- main.js
+++ main.js
@@ -1,10 +1,98 @@
-flap_holder = document.querySelector("#flap_holder")
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-class SplitFap {
-    constructor(target_index, content_array) {
+class SplitFlapDisplay {
+    constructor(parent, alphabet, rows, columns) {
+        this.rows = rows
+        this.columns = columns
+
+        const holder = document.createElement("div")
+        holder.className = "split_flap_holder"
+
+        this.holder = holder
+        this.row_elements = []
+
+        this.split_flaps = []
+
+        for (let j = 0; j < rows; j++) {
+            const row_element = document.createElement("div")
+            row_element.classList = "split_flap_row"
+            holder.appendChild(row_element)
+
+            this.row_elements.push(row_element)
+
+            this.split_flaps[j] = []
+            for (let i = 0; i < columns; i++) {
+                const new_split_flap = new SplitFlap(row_element, alphabet)
+
+                this.split_flaps[j][i] = new_split_flap
+            }
+        }
+
+        parent.appendChild(holder)
+    }
+
+    update_display() {
+        for (let j = 0; j < this.rows; j++) {
+            for (let i = 0; i < this.columns; i++) {
+                this.split_flaps[j][i].update()
+            }
+        }
+    }
+
+    async set_display(text) {
+        const words = text.split(' ')
+        const cursor = { row: 0, column: 0 }
+
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i]
+
+            if (cursor.column + word.length > this.columns) {
+                for (let j = cursor.column; j < this.columns; j++) {
+                    this.split_flaps[cursor.row][j].set_target_index(0)
+                }
+                cursor.row++
+                cursor.column = 0
+            }
+
+            if (cursor.row >= this.rows)
+                return
+
+            let x = cursor.column
+            let y = cursor.row
+
+            for (let j = 0; j < word.length; j++) {
+                let set = this.split_flaps[y][x + j].set_content(word[j])
+
+                if (set)
+                    await sleep(50)
+                else
+                    await sleep(10)
+            }
+
+            cursor.column += word.length + 1
+        }
+
+        for (let j = cursor.row; j < this.rows; j++) {
+            for (let i = 0; i < this.columns; i++) {
+                if (j === cursor.row && i < cursor.column - 1) {
+                    console.log(j, i)
+                    continue
+                }
+
+                this.split_flaps[j][i].set_target_index(0)
+                await sleep(10)
+            }
+        }
+    }
+}
+
+class SplitFlap {
+    constructor(parent, alphabet, target_index = 0) {
         this.current_index = 0
         this.target_index = target_index
-        this.content_array = content_array
+        this.alphabet = alphabet
 
         this.anim_durration = 100
 
@@ -16,10 +104,10 @@ class SplitFap {
         this.anim_top_flap = new_flap(this.element, "flap_top")
         this.anim_bottom_flap = new_flap(this.element, "flap_bottom")
 
-        this.set_flap_content(this.top_flap, this.content_array[this.current_index], "top")
-        this.set_flap_content(this.bottom_flap, this.content_array[this.current_index], "bottom")
-        this.set_flap_content(this.anim_top_flap, this.content_array[this.current_index], "top")
-        this.set_flap_content(this.anim_bottom_flap, this.content_array[this.current_index], "bottom")
+        this.set_flap_content(this.top_flap, this.alphabet[this.current_index], "top")
+        this.set_flap_content(this.bottom_flap, this.alphabet[this.current_index], "bottom")
+        this.set_flap_content(this.anim_top_flap, this.alphabet[this.current_index], "top")
+        this.set_flap_content(this.anim_bottom_flap, this.alphabet[this.current_index], "bottom")
 
         this.anim_top = this.anim_top_flap.flap.animate([{ transform: "scaleY(1)" }, { transform: "scaleY(0)" }], { duration: this.anim_durration, easing: "ease-in", fill: "forwards" })
         this.anim_bottom = this.anim_bottom_flap.flap.animate([{ transform: "scaleY(0)" }, { transform: "scaleY(1)" }], { duration: this.anim_durration, fill: "forwards", delay: this.anim_durration })
@@ -31,7 +119,7 @@ class SplitFap {
             await this.anim_top.finished
             await this.anim_bottom.finished
 
-            this.target_index = Math.round(Math.random() * this.content_array.length)
+            this.target_index = Math.round(Math.random() * this.alphabet.length)
         }
 
         this.element.onmouseover = async () => {
@@ -39,14 +127,14 @@ class SplitFap {
             await this.anim_bottom.finished
 
             this.current_index += 1
-            if (this.current_index >= this.content_array.length)
+            if (this.current_index >= this.alphabet.length)
                 this.current_index = 0
         }
 
-        flap_holder.appendChild(this.element)
+        parent.appendChild(this.element)
     }
 
-    update(dt) {
+    update() {
         if (this.current_index === this.target_index)
             return
 
@@ -55,7 +143,7 @@ class SplitFap {
                 this.anim_top.cancel()
                 this.anim_top_finished = true
             }
-            
+
             if (this.anim_bottom.currentTime > this.duration * 2) {
                 this.anim_bottom.cancel()
                 this.anim_bottom_finished = true
@@ -66,10 +154,10 @@ class SplitFap {
 
         let next_index = this.current_index + 1
 
-        if (next_index >= this.content_array.length)
+        if (next_index >= this.alphabet.length)
             next_index = 0
 
-        this.animate_flaps(this.content_array[this.current_index], this.content_array[next_index])
+        this.animate_flaps(this.alphabet[this.current_index], this.alphabet[next_index])
         this.current_index = next_index
     }
 
@@ -105,7 +193,7 @@ class SplitFap {
     }
 
     set_content(content) {
-        let i = this.content_array.indexOf(content)
+        let i = this.alphabet.indexOf(content)
 
         if (this.target_index === i)
             return false
@@ -140,15 +228,19 @@ const new_flap = (parent, className) => {
     }
 }
 
+const flap_holder = document.querySelector(".split_flap_holder")
+
 // str = " AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!."
-str = " etaoinshrdlcumwfgypbvkjxqzETAOINSHRDLCUMWFGYPBVKJXQZ0123456789!./:@"
+str = " etaoinshrdlcumwfgypbvkjxqzETAOINSHRDLCUMWFGYPBVKJXQZ0123456789!.,/:@"
 arr = str.split('')
 flaps = []
-for (let i = 0; i < 234; i++) {
-    let new_flap = new SplitFap(0, arr)
+// for (let i = 0; i < 234; i++) {
+//     let new_flap = new SplitFlap(flap_holder, arr)
+// 
+//     flaps.push(new_flap)
+// }
 
-    flaps.push(new_flap)
-}
+const split_flap_display = new SplitFlapDisplay(flap_holder, arr, 5, 30)
 
 let last_time = 0
 const loop = (tt) => {
@@ -157,16 +249,14 @@ const loop = (tt) => {
     let dt = last_time - tt
     dt = Math.max(dt, (1 / 60))
 
+    split_flap_display.update_display()
+
     flaps.forEach(flap => {
         flap.update(dt)
     })
     last_time = tt
 }
 loop(0)
-
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
 
 const textarea = document.querySelector("textarea")
 textarea.addEventListener("input", () => {
@@ -175,19 +265,7 @@ textarea.addEventListener("input", () => {
 
 const update_text = async () => {
     text = textarea.value
-
-    for (let i = 0; i < flaps.length; i++) {
-        if (i <= text.length) {
-            let is_set = flaps[i].set_content(text[i])
-
-            if (is_set)
-                await sleep(50)
-
-        } else {
-            flaps[i].set_target_index(0)
-            await sleep(10)
-        }
-    }
+    split_flap_display.set_display(text)
 }
 
 update_text()
